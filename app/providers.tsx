f083606bc7d8a94b89a54bf0653f0dbb8f649630
John Hu <huziyong@gmail.com>
2024-03-08 21:15:54 +0800

    Use a theme template

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,22 @@
 "use client";
 
-import { NextUIProvider } from "@nextui-org/react";
+import * as React from "react";
+import { NextUIProvider } from "@nextui-org/system";
+import { useRouter } from 'next/navigation'
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ThemeProviderProps } from "next-themes/dist/types";
 
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+export interface ProvidersProps {
+	children: React.ReactNode;
+	themeProps?: ThemeProviderProps;
+}
+
+export function Providers({ children, themeProps }: ProvidersProps) {
+  const router = useRouter();
+
+	return (
+		<NextUIProvider navigate={router.push}>
+			<NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+		</NextUIProvider>
+	);
 }
