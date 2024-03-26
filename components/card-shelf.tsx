e3990f032cc317e2942085e5af45af3ae9f13437
John Hu <huziyong@gmail.com>
2024-03-26 21:00:30 +0800

    Fix style of card-shelf

--- components/card-shelf.tsx
+++ components/card-shelf.tsx
@@ -8,7 +8,7 @@ const contents = contentsConfig.items;
 
 export const CardShelf = () => {
   return (
-    <div className="grid grid-cols-2 gap-2 sm:grid-cols-4">
+    <div className="grid max-w-[900px] grid-cols-12 grid-rows-2 gap-2 px-8">
       {contents.map((item, index) => (
         <Card
           className="col-span-12 h-[300px] sm:col-span-4"
@@ -16,7 +16,7 @@ export const CardShelf = () => {
           isPressable
           onPress={() => console.log("item pressed")}
         >
-          <CardHeader className="absolute top-1 z-10 flex-col !items-start">
+          <CardHeader className="absolute top-1 z-10 flex-col !items-start bg-gray-800/50">
             <p className="text-tiny font-bold uppercase text-white/60">
               {item.source}
             </p>
--- config/site.ts
+++ config/site.ts
@@ -1,69 +1,59 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Next.js + NextUI",
-	description: "Make beautiful websites regardless of your design experience.",
-	navItems: [
-		{
-			label: "Home",
-			href: "/",
-		},
+  name: "Next.js + NextUI",
+  description: "Make beautiful websites regardless of your design experience.",
+  navItems: [
     {
-      label: "Docs",
-      href: "/docs",
-    },
-    {
-      label: "Pricing",
-      href: "/pricing",
+      label: "Home",
+      href: "/",
     },
     {
       label: "Blog",
-      href: "/blog",
+      href: "https://hutusi.com/",
     },
     {
       label: "About",
-      href: "/about",
-    }
-	],
-	navMenuItems: [
-		{
-			label: "Profile",
-			href: "/profile",
-		},
-		{
-			label: "Dashboard",
-			href: "/dashboard",
-		},
-		{
-			label: "Projects",
-			href: "/projects",
-		},
-		{
-			label: "Team",
-			href: "/team",
-		},
-		{
-			label: "Calendar",
-			href: "/calendar",
-		},
-		{
-			label: "Settings",
-			href: "/settings",
-		},
-		{
-			label: "Help & Feedback",
-			href: "/help-feedback",
-		},
-		{
-			label: "Logout",
-			href: "/logout",
-		},
-	],
-	links: {
-		github: "https://github.com/nextui-org/nextui",
-		twitter: "https://twitter.com/getnextui",
-		docs: "https://nextui.org",
-		discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev"
-	},
+      href: "https://hutusi.com/about",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Profile",
+      href: "/profile",
+    },
+    {
+      label: "Dashboard",
+      href: "/dashboard",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "Team",
+      href: "/team",
+    },
+    {
+      label: "Calendar",
+      href: "/calendar",
+    },
+    {
+      label: "Settings",
+      href: "/settings",
+    },
+    {
+      label: "Help & Feedback",
+      href: "/help-feedback",
+    },
+    {
+      label: "Logout",
+      href: "/logout",
+    },
+  ],
+  links: {
+    github: "https://github.com/hutusi",
+    twitter: "https://twitter.com/hutusi",
+    sponsor: "https://github.com/hutusi",
+  },
 };
